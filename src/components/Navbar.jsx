4ecd6fabcf5b7671b94515356d1ca1cd82c96a7e
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-07-17 16:26:50 +0530

    Add: Cursor Pointer to the Navbar components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,11 +18,11 @@ const Navbar = () => {
                     <img src={logo} alt="Law Scholar's Logo" className="h-14 mr-4 ml-4" />
                     {/* <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>Law Scholars</h1> */}
                     <ul className='hidden md:flex'>
-                        <li><Link to="home" smooth={true} duration={500}>Home</Link></li>
-                        <li><Link to="about" smooth={true} offset={-200} duration={500}>About</Link></li>
-                        <li><Link to="registration" smooth={true} offset={-100} duration={500}>Registration</Link></li>
-                        <li><Link to="posts" smooth={true} offset={-50} duration={500}>Posts</Link></li>
-                        <li><Link to="support" smooth={true} offset={-50} duration={500}>Support</Link></li>
+                        <li className="cursor-pointer"><Link to="home" smooth={true} duration={500}>Home</Link></li>
+                        <li className="cursor-pointer"><Link to="about" smooth={true} offset={-200} duration={500}>About</Link></li>
+                        <li className="cursor-pointer"><Link to="registration" smooth={true} offset={-100} duration={500}>Registration</Link></li>
+                        <li className="cursor-pointer"><Link to="posts" smooth={true} offset={-50} duration={500}>Posts</Link></li>
+                        <li className="cursor-pointer"><Link to="support" smooth={true} offset={-50} duration={500}>Support</Link></li>
                     </ul>
                 </div>
                 <div className='hidden md:flex pr-4'>
