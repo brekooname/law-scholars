82100bd5a9da0af65739dc93579232f36f7951e5
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-07-17 12:09:40 +0530

    Update: Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,53 +1,53 @@
-import React from "react"
+import React, { useState } from "react"
+import { Link, animateScroll as scroll, } from 'react-scroll'
 
-import {MenuIcon, XIcon} from '@heroicons/react/solid'
+import { MenuIcon, XIcon } from '@heroicons/react/solid'
 
 const Navbar = () => {
+    const [nav, setNav] = useState(false)
+    const Clicking = () => setNav(!nav)
+
+    const Closing = () => setNav(!nav)
+
     return (
-        <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5  dark:bg-gray-900">
-        <div class="container flex flex-wrap justify-between items-center mx-auto">
-        <a href="#" class="flex items-center">
-            <img src={ require('../assets/law-scholars.png') } class="mr-3 h-6 sm:h-9" alt="Law Scholars Logo" />
-            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white sm:text-3xl">Law Scholars</span>
-        </a>
-        <div class="flex md:order-2">
-          <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1" >
-            <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
-            <span class="sr-only">Search</span>
-          </button>
-          <div class="hidden relative md:block">
-            <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-              <svg class="w-5 h-5 text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
-              <span class="sr-only">Search icon</span>
+        <div className='w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg'>
+            <div className='px-2 flex justify-between items-center w-full h-full'>
+                <div className='flex items-center'>
+                    <img src="../" alt="" />
+                    <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>Law Scholars</h1>
+                    <ul className='hidden md:flex'>
+                        <li><Link to="home" smooth={true} duration={500}>Home</Link></li>
+                        <li><Link to="about" smooth={true} offset={-200} duration={500}>About</Link></li>
+                        <li><Link to="support" smooth={true} offset={-50} duration={500}>Support</Link></li>
+                        <li><Link to="platforms" smooth={true} offset={-100} duration={500}>Platforms</Link></li>
+                        <li><Link to="pricing" smooth={true} offset={-50} duration={500}>Pricing</Link></li>
+                    </ul>
+                </div>
+                <div className='hidden md:flex pr-4'>
+                    <button className='px-4 py-2 border-none bg-transparent text-black mr-4'>
+                        Sign In
+                    </button>
+                    <button className='px-4 py-2'>Sign Up</button>
+                </div>
+                <div className='md:hidden mr-4' onClick={Clicking}>
+                    {!nav ? <MenuIcon className='w-5' /> : <XIcon className='w-5' />}
+
+                </div>
             </div>
-            <input type="text" id="search-navbar" class="block p-2 pl-10 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..."/>
-          </div>
-          <button data-collapse-toggle="navbar-search" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-search" aria-expanded="false">
-            <span class="sr-only">Open menu</span>
-            <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-          </button>
-        </div>
-          <div class="hidden justify-between items-center w-full md:flex md:w-auto md:order-1" id="navbar-search">
-            <div class="relative mt-3 md:hidden">
-              <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                <svg class="w-5 h-5 text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
-              </div>
-              <input type="text" id="search-navbar" class="block p-2 pl-10 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..."/>
-            </div>
-            <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
-              <li>
-                <a href="#" class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
-              </li>
-              <li>
-                <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Blogs</a>
-              </li>
-              <li>
-                <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Discussion</a>
-              </li>
+
+            <ul className={!nav ? 'hidden' : 'absolute bg-zinc-200 w-full px-8'}>
+                <li className='border-b-2 border-zinc-300 w-full'><Link onClick={Closing} to="home" smooth={true} duration={500}>Home</Link></li>
+                <li className='border-b-2 border-zinc-300 w-full'><Link onClick={Closing} to="about" smooth={true} offset={-200} duration={500}>About</Link></li>
+                <li className='border-b-2 border-zinc-300 w-full'><Link onClick={Closing} to="support" smooth={true} offset={-50} duration={500}>Support</Link></li>
+                <li className='border-b-2 border-zinc-300 w-full'><Link onClick={Closing} to="platforms" smooth={true} offset={-100} duration={500}>Platforms</Link></li>
+                <li className='border-b-2 border-zinc-300 w-full'><Link onClick={Closing} to="pricing" smooth={true} offset={-50} duration={500}>Pricing</Link></li>
+
+                <div className='flex flex-col my-4'>
+                    <button className='bg-transparent text-blue-500 px-8 py-3 mb-4'>Sign In</button>
+                    <button className='px-8 py-3'>Sign Up</button>
+                </div>
             </ul>
-          </div>
         </div>
-      </nav>
     )
 }
 
