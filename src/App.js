03238f3ce6a53a473cb7c28356fd3158bc5c088a
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-07-17 17:01:04 +0530

    Add: Links for all Navbar Items

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Registration from "./components/Registration";
+import Posts from "./components/Posts";
+import Support from "./components/Support";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Hero/>
       <About/>
       <Registration/>
+      <Posts/>
+      <Support/>
     </>
   );
 }
