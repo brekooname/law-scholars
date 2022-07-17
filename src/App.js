54cf1d6c255434a589594ce30acac942da0c4a7f
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-07-17 16:50:25 +0530

    Add: Link for Registration Section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Registration from "./components/Registration";
 
 function App() {
   return (
     <>
       <Navbar/>
       <Hero/>
+      <About/>
+      <Registration/>
     </>
   );
 }
