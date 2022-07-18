3fdb038667d67d3204fdb4ec46c88cb6c2537644
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-07-18 16:53:31 +0530

    Add: Searchbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,6 +25,12 @@ const Navbar = () => {
                         <li className="cursor-pointer"><Link to="support" smooth={true} offset={-50} duration={500}>Support</Link></li>
                     </ul>
                 </div>
+
+                <div className="hidden md:flex relative bg-zinc-300 p-2 rounded-md border-2 border-zinc-200 m-2">
+                    <svg class="w-5 h-5 left-1 absolute ml-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                    <input type="text" placeholder="Search..." className="ml-6 bg-transparent"/>
+                </div>
+                
                 <div className='hidden md:flex pr-4'>
                     <button className='px-4 py-2 border-none bg-transparent text-black mr-4'>
                         Sign In
