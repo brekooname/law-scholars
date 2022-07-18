345cc2f309f8d5b67a148e0d4dd3f0d9bc723d1d
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-07-18 18:22:05 +0530

    Update: Registration and Posts section

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ function App() {
       <Navbar/>
       <Hero/>
       <About/>
-      <Registration/>
-      <Posts/>
+      {/* <Registration/> */}
+      {/* <Posts/> */}
       <Support/>
       <Footer/>
     </>
