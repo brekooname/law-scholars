efdc68566065b5ee7eef74c7197302b0e658f4d7
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-07-19 17:08:58 +0530

    Update: Registration section for College info

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -4,7 +4,7 @@ const Registration = () => {
     return (
         <div name='registration' className='w-full my-44'>
             <div className='max-w-[1240px] mx-auto'>
-            <div className='text-center'>
+                <div className='text-center'>
                     <h2 className='text-5xl font-bold'>Student Registration</h2>
                 </div>
                 <div className="p-5 my-20">
@@ -28,7 +28,7 @@ const Registration = () => {
                                         <line x1="23" y1="11" x2="17" y2="11"></line>
                                     </svg>
                                 </div>
-                                <div className="absolute top-0 -ml-10 text-center mt-16 w-32 text-xs font-medium uppercase text-blue-500">Account</div>
+                                <div className="absolute top-0 -ml-10 text-center mt-16 w-32 text-xs font-medium uppercase text-blue-500">Educational</div>
                             </div>
                             <div className="flex-auto border-t-2 transition duration-500 ease-in-out border-gray-300"></div>
                             <div className="flex items-center text-gray-500 relative">
@@ -55,27 +55,27 @@ const Registration = () => {
                     </div>
                     <div className="mt-8 p-4">
                         <div>
-                            <div className="font-bold text-gray-600 text-xs leading-8 uppercase h-6 mx-2 mt-3">Full Name</div>
+                            <div className="font-bold text-gray-600 text-xs leading-8 uppercase h-6 mx-2 mt-3">Name of University</div>
                             <div className="flex flex-col md:flex-row">
                                 <div className="w-full flex-1 mx-2 svelte-1l8159u">
                                     <div className="bg-white my-2 p-1 flex border border-gray-200 rounded svelte-1l8159u">
-                                        <input placeholder="First Name" className="p-1 px-2 appearance-none outline-none w-full text-gray-800" /> </div>
+                                        <input placeholder="Christ University" className="p-1 px-2 appearance-none outline-none w-full text-gray-800" /> </div>
                                 </div>
                                 <div className="w-full flex-1 mx-2 svelte-1l8159u">
                                     <div className="bg-white my-2 p-1 flex border border-gray-200 rounded svelte-1l8159u">
-                                        <input placeholder="Last Name" className="p-1 px-2 appearance-none outline-none w-full text-gray-800" /> </div>
+                                        <input placeholder="Name of College" className="p-1 px-2 appearance-none outline-none w-full text-gray-800" /> </div>
                                 </div>
                             </div>
                             <div className="flex flex-col md:flex-row">
                                 <div className="w-full mx-2 flex-1 svelte-1l8159u">
-                                    <div className="font-bold h-6 mt-3 text-gray-600 text-xs leading-8 uppercase"> Username</div>
+                                    <div className="font-bold h-6 mt-3 text-gray-600 text-xs leading-8 uppercase"> Year of Study</div>
                                     <div className="bg-white my-2 p-1 flex border border-gray-200 rounded svelte-1l8159u">
-                                        <input placeholder="Just a hint.." className="p-1 px-2 appearance-none outline-none w-full text-gray-800" /> </div>
+                                        <input type={"number"} placeholder="Your Current Year" className="p-1 px-2 appearance-none outline-none w-full text-gray-800" /> </div>
                                 </div>
                                 <div className="w-full mx-2 flex-1 svelte-1l8159u">
                                     <div className="font-bold h-6 mt-3 text-gray-600 text-xs leading-8 uppercase"> Your Email</div>
                                     <div className="bg-white my-2 p-1 flex border border-gray-200 rounded svelte-1l8159u">
-                                        <input placeholder="jhon@doe.com" className="p-1 px-2 appearance-none outline-none w-full text-gray-800" /> </div>
+                                        <input type={"email"} placeholder="test@gmail.com" className="p-1 px-2 appearance-none outline-none w-full text-gray-800" /> </div>
                                 </div>
                             </div>
                         </div>
