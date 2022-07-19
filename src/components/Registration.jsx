bd83aee590145c1d899d9d6081e95144534df3d8
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-07-19 18:42:17 +0530

    Update: University name

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -59,7 +59,7 @@ const Registration = () => {
                             <div className="flex flex-col md:flex-row">
                                 <div className="w-full flex-1 mx-2 svelte-1l8159u">
                                     <div className="bg-white my-2 p-1 flex border border-gray-200 rounded svelte-1l8159u">
-                                        <input placeholder="Christ University" className="p-1 px-2 appearance-none outline-none w-full text-gray-800" /> </div>
+                                        <input placeholder="University Name" className="p-1 px-2 appearance-none outline-none w-full text-gray-800" /> </div>
                                 </div>
                                 <div className="w-full flex-1 mx-2 svelte-1l8159u">
                                     <div className="bg-white my-2 p-1 flex border border-gray-200 rounded svelte-1l8159u">
