5855ce7e255bf67fcfedba062140218e661721be
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-07-19 13:07:26 +0530

    Update: Padding for smaller screens

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,10 +6,10 @@ const About = () => {
             <div className='max-w-[1240px] mx-auto'>
                 <div className='text-center'>
                     <h2 className='text-5xl font-bold'>Trusted by Attorneys all around the world</h2>
-                    <p className='text-3xl py-6 text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum placeat assumenda nostrum sed, voluptatem nesciunt sit fuga illo totam provident non a, quam nihil est.</p>
+                    <p className='text-3xl text-gray-500 py-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum placeat assumenda nostrum sed, voluptatem nesciunt sit fuga illo totam provident non a, quam nihil est.</p>
                 </div>
 
-                <div className='grid md:grid-cols-3 gap-6 px-4 text-center'>
+                <div className='grid md:grid-cols-3 gap-6 px-4 text-center py-20'>
                     <div className='border py-8 rounded-xl shadow-xl'>
                         <p className='text-6xl font-bold text-blue-500 cursor-pointer'>10k</p>
                         <p className='text-gray-400 mt-2'>Happy Users</p>
