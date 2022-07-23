2470e941f59701f50093c552379bac69fa70207b
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-07-23 13:07:50 +0530

    Remove: Font Awesome link comments

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -114,8 +114,8 @@ const Blogs = () => {
                             </p>
                         </div>
                         <div className="flex items-center justify-center my-16 w-full">
-                    <button className=" hover:bg-blue-400 lg:text-2xl md:text-lg text-sm rounded f-m-m font-semibold text-white focus:outline-none lg:px-12 px-6 lg:py-6 py-3 xl:leading-4">Browse More</button>
-                </div>
+                            <button className=" hover:bg-blue-400 lg:text-2xl md:text-lg text-sm rounded f-m-m font-semibold text-white focus:outline-none lg:px-12 px-6 lg:py-6 py-3 xl:leading-4">Browse More</button>
+                        </div>
                     </div>
                 </section>
                 {/* <!-- Section: Design Block --> */}
