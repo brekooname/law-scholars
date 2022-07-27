7876a23851714d10087779fd75fb2d463e1516a1
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-07-28 01:25:01 +0530

    Initial commit for Quiz section

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Quiz = () => {
+    return (
+        <div name='quiz' className='w-full my-32'>
+            <div className='max-w-[1240px] mx-auto'>
+                <div className='text-center'>
+                    <h2 className='text-5xl font-bold'>Quiz Section</h2>
+                    <p className='text-3xl text-gray-500 py-10'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam, ipsum?</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Quiz
