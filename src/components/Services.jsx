d61ebb66a442d1641cd4def2257e12c0a008381c
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-07-31 00:46:20 +0530

    Update: Rename Support to Services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -13,16 +13,16 @@ import {
 
 import supportImage from '../assets/support.jpg'
 
-const Support = () => {
+const Services = () => {
     return (
-        <div name='support' className='w-full mt-36'>
+        <div name='service' className='w-full mt-36'>
             <div className='w-full h-[700px] bg-gray-900/90 absolute'>
                 <img className='w-full h-full object-cover mix-blend-overlay' src={supportImage} alt="/" />
             </div>
 
             <div className='max-w-[1240px] mx-auto text-white relative'>
                 <div className='px-4 py-12'>
-                    <h2 className='text-3xl pt-8 text-slate-300 uppercase text-center'>Support</h2>
+                    <h2 className='text-3xl pt-8 text-slate-300 uppercase text-center'>Services</h2>
                     <h3 className='text-5xl font-bold py-6 text-center'>Finding the right team</h3>
                     <p className='py-4 text-3xl text-slate-300'>Lorem ipsum dolor sit amet consectetur adipisicing elit. In repudiandae veritatis ratione error tenetur, voluptates architecto possimus ad! Omnis minima ea quidem quisquam unde beatae, minus illo et cum vel?</p>
                 </div>
@@ -64,4 +64,4 @@ const Support = () => {
     )
 }
 
-export default Support
+export default Services
