d88e41028a7603ba16301e5af9a08759dd3b75fa
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-08-01 20:58:33 +0530

    Update: Hero background image

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -17,9 +17,9 @@ import bgImg from '../assets/law-books.jpg'
 
 const Hero = () => {
     return (
-        <div name="home" className="w-full h-screen bg-zinc-200 flex flex-col justify-between">
-            <div className="grid md:grid-cols-2 max-w-[1240px] m-auto">
-                <div className="flex flex-col justify-center md:items-start w-full px-2 py-8">
+        <div name="home" className="w-full h-screen bg-[url('https://images.unsplash.com/photo-1568667256549-094345857637?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1915&q=80')] flex flex-col justify-center bg-cover bg-center">
+            {/* <div className="grid md:grid-cols-2 max-w-[1240px] m-auto"> */}
+                <div className="flex flex-col justify-center md:items-start sm:w-[50%] xsm:text-slate-300 px-2 py-8 m-8">
                     <p className="text-2xl">Think like a Lawyer</p>
                     <p className="text-2xl">Lock the Trial</p>
                     <p className="text-2xl">Get Justice</p>
@@ -32,13 +32,10 @@ const Hero = () => {
                             loop
                         /></p>
                     <button className="scroll-smooth py-3 px-6 my-4 sm:w-[60%]"><Link to="registration" smooth={true} duration={500}>Get Started</Link></button>
-                </div>
-                <div>
-                    <img className="w-full rounded-lg" src={bgImg} alt="https://unsplash.com" />
-                    {/* <img className="w-full" src="https://source.unsplash.com/random/500x500/?law,court" alt="Random Law related image from Unsplash" /> */}
-                </div>
-                <div className='absolute flex flex-col py-8 bottom-[5%] mx-1 transform bg-zinc-200 border border-slate-300 rounded-xl text-center shadow-xl xsm:-my-20 xsm:bottom-[-3%] md:min-w-[760px]  md:left-1/2 md:-translate-x-1/2 md:bottom-[20%] lg:bottom-[15%]'>
-                    <p>Check out our Community:</p>
+                {/* </div> */}
+               
+                <div className='absolute flex flex-col py-8 bottom-[5%] mx-1 transform bg-zinc-200 border border-slate-300 rounded-xl text-center shadow-xl xsm:-my-20 xsm:bottom-[-3%] xsm:mr-10  md:min-w-[760px]  md:left-1/2 md:-translate-x-1/2 md:bottom-[20%] lg:bottom-[15%]'>
+                    <p className="text-black">Check out our Community:</p>
                     <div className='flex justify-between flex-wrap px-4'>
                         <p className='flex px-4 py-2 text-slate-500'><FontAwesomeIcon className="h-6 text-blue-500 mr-2" icon={faPeopleGroup} /> Live Discussion</p>
                         <p className='flex px-4 py-2 text-slate-500'><FontAwesomeIcon className="h-6 text-blue-500 mr-2" icon={faScaleUnbalanced} /> Current Legal Affairs</p>
