3cdbb08f260189bb37795baf983d648631aa308a
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-08-02 13:02:42 +0530

    Update: Color

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { Link, animateScroll as scroll, } from 'react-scroll'
 import { MenuIcon, XIcon } from '@heroicons/react/solid'
 
 import logo from '../assets/new-law-scholars.png'
+import newLogo from '../assets/new-logo.png'
 
 const Navbar = () => {
     const [nav, setNav] = useState(false)
@@ -12,26 +13,26 @@ const Navbar = () => {
     const Closing = () => setNav(!nav)
 
     return (
-        <div className='w-screen h-[80px] z-10 bg-slate-200 fixed drop-shadow-lg'>
+        <div className='w-screen h-[80px] z-10 bg-[#653818] fixed drop-shadow-lg'>
             <div className='px-2 flex justify-between items-center w-full h-full'>
-                <div className='flex items-center'>
-                    <Link to="home" smooth={true} duration={500}><img src={logo} alt="Law Scholar's Logo" className="h-20 mr-4 ml-4 cursor-pointer" /></Link>
+                <div className='flex items-center text-[#ede0d4]'>
+                    <Link to="home" smooth={true} duration={500}><img src={newLogo} alt="Law Scholar's Logo" className="h-20 mr-4 ml-4 cursor-pointer" /></Link>
                     {/* <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>Law Scholars</h1> */}
                     <ul className='hidden md:flex'>
-                        <li className="cursor-pointer hover:text-blue-500"><Link to="home" smooth={true} duration={500}>Home</Link></li>
-                        <li className="cursor-pointer hover:text-blue-500"><Link to="about" smooth={true} offset={-200} duration={500}>About</Link></li>
-                        <li className="cursor-pointer hover:text-blue-500"><Link to="quiz" smooth={true} offset={-200} duration={500}>Quiz</Link></li>
-                        <li className="cursor-pointer hover:text-blue-500"><Link to="registration" smooth={true} offset={-100} duration={500}>Registration</Link></li>
-                        <li className="cursor-pointer hover:text-blue-500"><Link to="blogs" smooth={true} offset={-50} duration={500}>Blogs</Link></li>
-                        <li className="cursor-pointer hover:text-blue-500"><Link to="livechat" smooth={true} offset={-50} duration={500}>Discussion</Link></li>
-                        <li className="cursor-pointer hover:text-blue-500"><Link to="testimonial" smooth={true} offset={-50} duration={500}>Testimonials</Link></li>
-                        <li className="cursor-pointer hover:text-blue-500"><Link to="service" smooth={true} offset={-50} duration={500}>Services</Link></li>
+                        <li className="cursor-pointer hover:text-[#004284]"><Link to="home" smooth={true} duration={500}>Home</Link></li>
+                        <li className="cursor-pointer hover:text-[#004284]"><Link to="about" smooth={true} offset={-200} duration={500}>About</Link></li>
+                        <li className="cursor-pointer hover:text-[#004284]"><Link to="quiz" smooth={true} offset={-200} duration={500}>Quiz</Link></li>
+                        <li className="cursor-pointer hover:text-[#004284]"><Link to="registration" smooth={true} offset={-100} duration={500}>Registration</Link></li>
+                        <li className="cursor-pointer hover:text-[#004284]"><Link to="blogs" smooth={true} offset={-50} duration={500}>Blogs</Link></li>
+                        <li className="cursor-pointer hover:text-[#004284]"><Link to="livechat" smooth={true} offset={-50} duration={500}>Discussion</Link></li>
+                        <li className="cursor-pointer hover:text-[#004284]"><Link to="testimonial" smooth={true} offset={-50} duration={500}>Testimonials</Link></li>
+                        <li className="cursor-pointer hover:text-[#004284]"><Link to="service" smooth={true} offset={-50} duration={500}>Services</Link></li>
                     </ul>
                 </div>
 
-                <div className="hidden md:flex relative bg-zinc-300 p-2 rounded-md border-2 border-zinc-200 m-2">
-                    <svg class="w-5 h-5 left-1 absolute ml-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-                    <input type="text" placeholder="Search..." className="ml-6 bg-transparent focus:outline-none" />
+                <div className="hidden md:flex relative bg-transparent p-2 rounded-md border-2 border-[#552e13] m-2">
+                    <svg class="w-5 h-5 left-1 absolute ml-1 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                    <input type="text" placeholder="Search..." className="ml-6 bg-transparent focus:outline-none border-none text-white" />
                 </div>
 
 
