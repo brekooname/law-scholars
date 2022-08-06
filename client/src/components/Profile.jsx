3e453051c4725dc2d1fcc08c8e826688ff60b104
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-08-06 23:39:17 +0530

    Add: Separate page for Profile section

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
 export default function Profile() {
+
     return (
-        <div name='profile' className='w-full my-32'>
-            <h1 className='text-center'>Profile</h1>
+        <div>
+            <h1>Profile</h1>
         </div>
     )
 }
