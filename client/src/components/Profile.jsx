33788ce95a86ec0eae22a18a027e6461c49bb563
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-08-06 15:10:28 +0530

    Add: Profile Page

---
+++ client/src/components/Profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profile = () => {
+    return (
+        <div name='profile' className='w-full my-32'>
+            <h1>Profile</h1>
+        </div>
+    )
+}
+
+export default Profile
