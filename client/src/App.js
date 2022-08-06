8760e0f3e3d4a57c3b80465082083044d489c949
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-08-06 23:04:43 +0530

    Update: Routes for SPA

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Routes, Redirect } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
@@ -16,63 +16,54 @@ import Quiz from "./components/Quiz";
 import logo from "./assets/logo.jpg";
 import Profile from "./components/Profile";
 
-class App extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = { apiResponse: "" };
-  }
-
-  callAPI() {
-    fetch("http://localhost:9000/testAPI")
-      .then(res => res.text())
-      .then(res => this.setState({ apiResponse: res }))
-      .catch(err => err);
-  }
-
-  componentDidMount() {
-    this.callAPI();
-  }
-
-  render() {
-
-    return (
-      <>
-        {/* <Routes> */}
-          {/* <Route path="/LoginSignup" element={<LoginSignup />} /> */}
-        {/* </Routes> */}
-
-        {/* <BrowserRouter>
-          <Route path='/LoginSignup' element={<LoginSignup />}  />
-          <Route path='/' element={<Hero />} />
-          <Route path='/About' element={<About />} />
-          <Route path='/Registration' element={<Registration />} />
-          <Route path='/Blogs' element={<Blogs />} />
-          <Route path='/Services' element={<Services />} />
-          <Route path='/Footer' element={<Footer />} />
-          <Route path='/Testimonial' element={<Testimonial />} />
-          <Route path='/LiveChat' element={<LiveChat />} />
-          <Route path='/Quiz' element={<Quiz />} />
-        </BrowserRouter> */}
-
-        <Navbar />
-        <Hero />
-        <About />
-        <Quiz />
-        <Registration />
-        <Blogs />
-        <LiveChat />
-        <Testimonial />
-        <Services />
-        {/* <LoginSignup /> */}
-        <Profile />
-        <Footer />
-      </>
-    );
-  }
+const HomePage = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Quiz />
+      <Registration />
+      <Blogs />
+      <LiveChat />
+      <Testimonial />
+      <Services />
+      <Footer />
+    </>
+  )
 }
 
-export default App;
+export default function App() {
+
+  // const [apiResponse, setApiResponse] = useState("")
+
+  // useEffect(() => {
+
+  //   const callAPI = async () => {
+  //     const response = await fetch('http://localhost:9000/testAPI')
+  //     const text = await response.text()
+  //     setApiResponse(text)
+  //   }
+
+  //   callAPI()
+  //   return () => setApiResponse('')
+  // }, [])
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={
+          <div>
+            Error 404
+          </div>
+        } />
+        <Route path="/law-scholars" element={<HomePage />} />
+      </Routes>
+
+      {/* Other routes */}
+    </BrowserRouter>
+  )
+}
 
 
 // From line 37 to test Server:
@@ -83,3 +74,21 @@ export default App;
 //</header>
 //<p className="App-intro">{this.state.apiResponse}</p>
 //</div>
+
+
+
+// constructor(props) {
+//   super(props);
+//   this.state = { apiResponse: "" };
+// }
+
+// callAPI() {
+//   fetch("http://localhost:9000/testAPI")
+//     .then(res => res.text())
+//     .then(res => this.setState({ apiResponse: res }))
+//     .catch(err => err);
+// }
+
+// componentDidMount() {
+//   this.callAPI();
+// }
