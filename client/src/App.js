b5851789126cd52833bfbb6d29ab88566ddc9373
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-08-07 12:00:52 +0530

    Add: Error 404 Page

--- client/src/App.js
+++ client/src/App.js
@@ -33,6 +33,28 @@ const HomePage = () => {
   )
 }
 
+const ErrorPage = () => {
+  return (
+    <>
+      <div class="bg-blue-400 relative overflow-hidden h-screen">
+        <img src="https://external-preview.redd.it/4MddL-315mp40uH18BgGL2-5b6NIPHcDMBSWuN11ynM.jpg?width=960&crop=smart&auto=webp&s=b98d54a43b3dac555df398588a2c791e0f3076d9" class="absolute h-full w-full object-cover" />
+        <div class="inset-0 bg-black opacity-25 absolute">
+        </div>
+        <div class="container mx-auto px-6 md:px-12 relative z-10 flex items-center py-20 xl:py-24">
+          <div class="w-full font-mono flex flex-col items-center relative z-10">
+            <h1 class="font-extrabold text-5xl text-center text-white leading-tight mt-4">
+              You are all alone here
+            </h1>
+            <p class="text-center font-extrabold text-8xl xsm:my-[450px]  md:my-10 text-white animate-bounce">
+              404
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
 export default function App() {
 
   // const [apiResponse, setApiResponse] = useState("")
@@ -52,11 +74,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="*" element={
-          <div>
-            Error 404
-          </div>
-        } />
+        <Route path="*" element={<ErrorPage />} />
         <Route path="/law-scholars" element={<HomePage />} />
         <Route path="/law-scholars/login" element={<LoginSignup />} />
         <Route path="/law-scholars/profile" element={<Profile />} />
