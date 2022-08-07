e3d2c122d9eccc6e13d6904e3a041005270c3d5e
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-08-07 12:56:04 +0530

    Add: Navbar for User Profile Page

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useState } from "react";
 
-export default function Profile() {
+export default function Index() {
+    const [show, setShow] = useState(false);
 
     return (
-        <div>
-            <h1>Profile</h1>
+        <div className="bg-gray-50">
+            <div className="bg-white xl:hidden flex text-gray-800  hover:text-black focus:outline-none focus:text-black justify-between w-full p-6 items-center ">
+                <button className="flex justify-between  items-center space-x-3 bg-transparent text-black border-none hover:bg-transparent">
+                    <p className="text-2xl leading-6 ">Law Scholars</p>
+                </button>
+            </div>
         </div>
-    )
+    );
 }
