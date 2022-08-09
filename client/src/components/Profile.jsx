6a2d4ad82e43dd4d35b5e3d94af3209c5885ce1f
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-08-10 00:29:45 +0530

    Add: Law Scholars to homepage

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
+import { Link } from 'react-router-dom'
+
 import logo from '../assets/new-law-scholars.png'
 
 export default function Profile(props) {
 
-  const [navbarOpen, setNavbarOpen] = React.useState(false);
+    const [navbarOpen, setNavbarOpen] = React.useState(false);
 
     return (
         <>
@@ -14,7 +16,20 @@ export default function Profile(props) {
                         : "relative shadow-lg bg-white") +
                     " flex flex-wrap items-center justify-between px-2 py-3 "
                 }
-            ></nav>
+            >
+                <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+                    <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+                        <Link to="/law-scholars"
+                            className={
+                                (props.transparent ? "text-white" : "text-gray-800") +
+                                " text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
+                            }
+                        >
+                            Law Scholars
+                        </Link>
+                    </div>
+                </div>
+            </nav>
         </>
     );
 }
