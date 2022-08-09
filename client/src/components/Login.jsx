31a8647bbd973d67aff794aa951c50df53f895f0
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-08-09 18:13:03 +0530

    Update: Text center to only login/signup

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,11 +1,11 @@
 import axios from 'axios';
 import React, { Component } from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import './login.css';
 
 
 class Login extends Component {
-   
+
   handleSubmit = e => {
     e.preventDefault();
 
@@ -22,52 +22,32 @@ class Login extends Component {
       console.log(err)
     })
   }
- 
-  render(){
+
+  render() {
     return (
       <>
-      <div className='mega-container'>
-           <div className='sign-in-container'>
-         <form action='' onSubmit={this.handleSubmit}>
-          <div>
-              <h1>Sign in</h1>
-              <input type = "text" name ="email" id="email" placeholder='Username'
-              onChange={e => this.email = e.target.value} /><br></br>
-              <input type = "text" name ="password" id="password" placeholder='Password'
-              onChange={e => this.password = e.target.value} />
-              <br></br>
-  
-              <Link to="/law-scholars"><button className='login-btn'>Login</button></Link><br></br>
-              
-              <button className='forget-pass'>Forgot Password?</button>
-  
-              
-              
-             
-              
+        <div className='mega-container text-center'>
+          <div className='sign-in-container'>
+            <form action='' onSubmit={this.handleSubmit}>
+              <div>
+                <h1>Sign in</h1>
+                <input type="text" name="email" id="email" placeholder='Username'
+                  onChange={e => this.email = e.target.value} /><br></br>
+                <input type="text" name="password" id="password" placeholder='Password'
+                  onChange={e => this.password = e.target.value} />
+                <br></br>
+                <Link to="/law-scholars"><button className='login-btn'>Login</button></Link><br></br>
+                <button className='forget-pass'>Forgot Password?</button>
+              </div>
+            </form>
           </div>
-       </form>
-     
-     </div>
-  
-     <div className='sign-up-card'>
-      <h2>Don't have an Account?</h2>
-      <Link to="/signup"><button> Sign up </button></Link>
-  
-      
-  
-  
-     </div>
-      </div>
-  
+          <div className='sign-up-card'>
+            <h2>Don't have an Account?</h2>
+            <Link to="/signup"><button> Sign up </button></Link>
+          </div>
+        </div>
       </>
     )
   }
-  
 }
-   
-
- 
- 
-
 export default Login
