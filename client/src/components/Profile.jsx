7d22aaa56731cf23a94060f5c4f428e46c896862
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-08-12 01:04:19 +0530

    Update: Spacing for smaller screens

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -164,7 +164,7 @@ export default function Profile(props) {
                                     <div className="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
                                         <div className="py-6 px-3 mt-32 sm:mt-0">
                                             <button
-                                                className="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none xsm:ml-[45px]  sm:mr-2 mb-1"
+                                                className="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none xsm:ml-[47px] xsm:mt-[20px] sm:mr-2 mb-1"
                                                 type="button"
                                                 style={{ transition: "all .15s ease" }}
                                             >
@@ -199,6 +199,7 @@ export default function Profile(props) {
                                     <h3 className="text-4xl font-semibold leading-normal mb-2 text-gray-800 text-center">
                                         Ranjan Sharma
                                     </h3>
+                                    <br />
                                     <div className="text-sm leading-normal mt-0 mb-2 text-gray-500 font-bold uppercase">
                                         <FontAwesomeIcon icon={faMapMarkerAlt} className="mr-2 text-lg text-gray-500"></FontAwesomeIcon>
                                         Patna, Bihar
