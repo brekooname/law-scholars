0507c1adb0ada93c95578dd53cc34d90fe312613
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-08-12 00:58:59 +0530

    Update: Center for smaller screens

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -17,7 +17,7 @@ import {
     faUniversity,
 
 } from "@fortawesome/free-solid-svg-icons";
-import { faFacebook, faInstagram, faTwitch, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faFacebook, faInstagram, faTwitter } from "@fortawesome/free-brands-svg-icons";
 
 
 export default function Profile(props) {
@@ -26,7 +26,8 @@ export default function Profile(props) {
 
     return (
         <>
-            <nav transparent
+        {/* TODO: Set navbar transparent to true and white on scroll */}
+            <nav transparent={true} 
                 className={
                     (props.transparent
                         ? "top-0 absolute z-50 w-full"
@@ -155,7 +156,7 @@ export default function Profile(props) {
                                             <img
                                                 alt="..."
                                                 src={'https://source.unsplash.com/random/?portrait'}
-                                                className="shadow-xl rounded-full h-[200px] w-[200px] align-middle border-none absolute xsm:ml-[-108px] -m-16 -ml-20 lg:-ml-[100px]"
+                                                className="shadow-xl rounded-full h-[200px] w-[200px] align-middle border-none absolute xsm:ml-[-95px] -m-16 -ml-20 lg:-ml-[100px]"
                                                 style={{ maxWidth: "190px" }}
                                             />
                                         </div>
@@ -163,7 +164,7 @@ export default function Profile(props) {
                                     <div className="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
                                         <div className="py-6 px-3 mt-32 sm:mt-0">
                                             <button
-                                                className="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1"
+                                                className="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none xsm:ml-[45px]  sm:mr-2 mb-1"
                                                 type="button"
                                                 style={{ transition: "all .15s ease" }}
                                             >
