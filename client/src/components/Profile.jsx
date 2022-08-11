059aa6e5ca9dd054fbaaa3c63cb288c5caf63034
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-08-12 00:40:03 +0530

    Add: Footer for User Profile

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -236,6 +236,37 @@ export default function Profile(props) {
                 </section>
             </main>
 
+            <footer className="relative bg-gray-300 pt-8 pb-6">
+                <div
+                    className="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20"
+                    style={{ height: "80px" }}
+                >
+                    <svg
+                        className="absolute bottom-0 overflow-hidden"
+                        xmlns="http://www.w3.org/2000/svg"
+                        preserveAspectRatio="none"
+                        version="1.1"
+                        viewBox="0 0 2560 100"
+                        x="0"
+                        y="0"
+                    >
+                        <polygon
+                            className="text-gray-300 fill-current"
+                            points="2560 0 2560 100 0 100"
+                        ></polygon>
+                    </svg>
+                </div>
+                <div className="container mx-auto px-4">
+                    <div className="flex flex-wrap">
+                        <div className="w-full lg:w-6/12 px-4">
+                            <h4 className="text-3xl font-semibold">
+                                Let's keep in touch!
+                            </h4>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+
         </>
     );
 }
