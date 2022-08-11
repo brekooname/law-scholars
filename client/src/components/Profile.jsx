cfaeb365db041e961662b2c4eafff3c557555ff5
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-08-12 00:15:08 +0530

    Add: Followers section

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -168,6 +168,16 @@ export default function Profile(props) {
                                             </button>
                                         </div>
                                     </div>
+                                    <div className="w-full lg:w-4/12 px-4 lg:order-1">
+                                        <div className="flex justify-center py-4 lg:pt-4 pt-8">
+                                            <div className="mr-4 p-3 text-center">
+                                                <span className="text-xl font-bold block uppercase tracking-wide text-gray-700">
+                                                    10
+                                                </span>
+                                                <span className="text-sm text-gray-500">Followers</span>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
